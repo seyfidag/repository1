7d9ea80f68de5f32957c85fd7e3b9cff82396bbe
seyfidag <seyfidag3434@gmail.com>
2021-12-19 23:00:59 +0300

    2. yorum

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function func1(params) {
     return params
 }
+
+
+let yeniDizi=[1,2,3,"ABC"]
+
+console.log(yeniDizi)
+
+
+
+let yeniDizi2 = new Array(5)
+console.log(yeniDizi2)
+
+let yeniDizi3 = new Array(15).fill(2)
+yeniDizi3[3]=5
+console.log(yeniDizi3)
+
